99bef497d8f89c80c453f0feceb92373a3114121
rahulXL1 <uploaderrahul@gmail.com>
2025-09-10 16:13:41 +0530

    To Do List

---
+++ script.js
@@ -0,0 +1,53 @@
+let todoList= [];
+
+renderTodoList();
+function renderTodoList() {
+  let todoListHTM = '';
+
+  todoList.forEach((todoObject) => {
+    const { name, dueDate } = todoObject;
+    const htm = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button class="delete-todo-button js-delete-todo-button">Delete</button> 
+    `;
+    todoListHTM += htm;
+  });
+
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTM;
+
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
+}
+
+document.querySelector('.js-add-todo-button')
+  .addEventListener('click', () => {
+    addTodo();
+  });
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    name,
+    dueDate
+  });
+
+  inputElement.value = '';
+
+  renderTodoList();
+}
+
+
+
